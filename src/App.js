3cb1d17bf0d928cd60857ed385d5556a8cd2edb9
Eric Bishard <e.bishard@gmail.com>
2018-08-27 07:38:19 -0700

    Ability to parse query parameters

--- src/App.js
+++ src/App.js
@@ -20,21 +20,56 @@ const Links = () => (
       *note `isActive()` function takes the args: match and location
     */}
     <NavLink isActive={isActiveFunc} activeClassName="active" to="/contact">Contact</NavLink>
+
+    <br/><br/>
+
+    {/* Parse query parameters inline and as object */}
+    <NavLink to="/query?id=123">inline</NavLink>
+    <NavLink to={{pathname:'/query', search:'id=456'}}>object</NavLink>
+
   </nav>
 )
 
 const App = () => (
   <Router>
     <div id="routes">
+
+      {/* Render our Links component (navigation) */}
       <Links />
+
       {/* Render component with component prop */}
       <Route exact path="/" component={Home} />
+
+      <Route exact path="/other/:page?-:subpage" render={({match}) => (
+        <h1>
+          Page: {match.params.page || 'Home'}<br/>
+          Subpage: {match.params.subpage}
+          </h1>
+      )} />
+
+      {/* Use Regexes with route params like: http://localhost:3000/regex/12-18-2018/.html*/}
+      <Route exact path="/regex/:a(\d{2}-\d{2}-\d{4})/:b(\.[a-z]+)" render={({match}) => (
+        <h1>
+          ParamA: {match.params.a}<br/>
+          ParamB: {match.params.b}
+          </h1>
+      )} />
 
       {/* Render component without component prop */}
       <Route path="/about" render={() => <h1>About</h1>} />
       
       {/* Render component using children with match argument */}
       <Route path="/contact" children={({match}) => match && <h1>Contact</h1>} />
+
+      {/* Parse Query parameters */}
+      <Route path="/query" render={({match, location}) =>(
+        <div>
+          <p>root</p>
+          <p>{JSON.stringify(match)}</p>
+          <p>{JSON.stringify(location)}</p>
+          <p>{new URLSearchParams(location.search).get('id')}</p>
+        </div>
+      )} />
 
     </div>
   </Router>
